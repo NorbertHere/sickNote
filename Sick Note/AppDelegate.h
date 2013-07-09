6816ec636a18876e7d3e2c4f3c1b4636a035906c
Yoshiko <jessica_fang88@hotmail.com>
2013-07-09 17:01:16 +0800

    Initial Commit

---
+++ Sick Note/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sick Note
+//
+//  Created by Lim Jessica on 9/7/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
